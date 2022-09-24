06d90472efb8de62bc502f6b076460b5412daaf8
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2022-09-24 06:27:00 +0530

    Solution

---
+++ Zeros and Ones Solution.py
@@ -0,0 +1,12 @@
+#  HAcker Rank Zeros and Ones Solution.................
+#  Read input from STDIN. Print output to STDOUT
+import numpy
+nums = tuple(map(int, input().split()))
+print(numpy.zeros(nums, dtype=numpy.int))
+print(numpy.ones(nums, dtype=numpy.int))
+
+
+
+#----------------------------------CODE INFINITY-------------------------------------------#
+#------------------------------SUBSCRIBE YOUR CHANNEL--------------------------------------#
+#----------------------FOR SOLUTION TUTORIALS AND MORE UPDATES-----------------------------#
